1e23ff04fdc5fa8687b462e4df0b52d1abbf00c0
AlexandreVerhoye <alexandreverhoye@gmail.com>
2017-10-25 08:43:56 +0200

    Dico. variables + base ecran combat

---
+++ EcranCombat/EcranCombat.dpr
@@ -0,0 +1,23 @@
+program EcranCombat;
+
+{$APPTYPE CONSOLE}
+
+{$R *.res}
+
+uses
+  System.SysUtils;
+
+var
+cpt : Integer; {Variable de type entier comportant le nombre de tour}
+typeComnbat : string; {Variable de type texte comportant le type de combat selectionné affiché a l'ecran);
+
+begin
+ writeln;
+ write('Civilisation : France');
+ write('                                                                                        ');
+ write('Tour : ', cpt);
+ writeln;
+ writeln;
+ writeln;
+ writeln('Combat contre : ', typeCombat);
+end.
